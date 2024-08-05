0e99830d93631c480c0fe6b447cf63651652f482
TanHop13 <tanhop1305@gmail.com>
2024-08-05 10:49:44 +0700

    update 5/8

--- JS/jsContact.js
+++ JS/jsContact.js
@@ -1,17 +1,4 @@
 let image = ''
-//Scroll to top
-let mysBtn = document 
-	.getElementById('scrollbuttonid'); 
-
-window.addEventListener('scroll', function () { 
-	if (document.body.scrollTop > 20 
-		|| document.documentElement.scrollTop > 20) { 
-		mysBtn.style.display = 'block'; 
-	} else { 
-		mysBtn.style.display = 'none'; 
-	} 
-});
-
 // Kiểm tra file ảnh
 document.getElementById('file').addEventListener('change', function(event) {
     const fileInput = event.target;
---
+++ JS/jscripe.js
@@ -0,0 +1,23 @@
+function init_main() {
+	$(window).scroll(function(){
+		var y = $(this).scrollTop();
+		$("#goToTop").toggleClass('slide-up', y > 400);
+
+	});
+	
+	$("#goToTop").click(function(){
+		$("html, body").animate({
+			scrollTop: 0
+		}, 900)
+	})
+
+	$(document).ready(function() {
+        $("a.delay-link").click(function(event) {
+            event.preventDefault();
+            var url = $(this).attr("href");
+            setTimeout(function() {
+                window.location.href = url;
+            }, 1000);
+        });
+    });
+}
